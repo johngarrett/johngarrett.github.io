2ff2fe10978b8f38dc07bda70ae08adb1cbacfb1
garrepi <jacgarrett18@gmail.com>
2026-04-14 16:33:47 -0700

    add craigslist style

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { readProjects } from "./projects";
 import { ProjectPages } from "./projects/project-pages";
 import { ProjectsPage } from "./projects/projects-page";
-import { RootPage } from "./root/root-page";
+import { HomePage } from "./root/root-page";
 import { StyleSheet } from "./styles/styles";
 import { build } from "./utils";
 
@@ -9,7 +9,7 @@ const projects = await readProjects("content/projects");
 
 const renderables = [
   // main
-  RootPage,
+  HomePage,
   // projects
   ProjectsPage(projects),
   ...ProjectPages(projects),
--- src/root/root-page.ts
+++ src/root/root-page.ts
@@ -1,7 +1,7 @@
 import { htmlPage } from "../components";
 import { html, type Renderable } from "../utils";
 
-export const RootPage: Renderable = {
+export const HomePage: Renderable = {
   path: "/index.html",
   render: () =>
     htmlPage({
@@ -9,6 +9,20 @@ export const RootPage: Renderable = {
         head: { title: "root page" },
         navbar: { selected: "home" },
       },
-      content: html` <div>garrepi.dev</div> `,
+      content: html`
+        <div class="home-container">
+          <div class="home-title">garrepi.dev</div>
+          <div class="home-link-boxes">
+            <div class="home-link-box">
+              <div class="home-link-box-title">foo section</div>
+              <a>foo</a>
+              <a>foo</a>
+              <a>foo</a>
+            </div>
+            <div class="home-link-box"></div>
+            <div class="home-link-box"></div>
+          </div>
+        </div>
+      `,
     }),
 };
