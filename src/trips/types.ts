049e755f1b119d9625df7a9d8504b9e0c7007e5c
garrepi <jacgarrett18@gmail.com>
2026-04-15 20:57:08 -0700

    add trip info

--- src/trips/types.ts
+++ src/trips/types.ts
@@ -1,7 +1,13 @@
 import type { MarkdownString } from "../utils";
 
+export type TripInfo = {
+  galleryResources: string;
+  mapResources?: string;
+  title: string;
+};
 export type Trip = {
   title: string;
   path: string;
   markdownContent: MarkdownString;
+  info: TripInfo;
 };
