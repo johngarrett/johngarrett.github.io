8d856e2626dead2a3160db846655d9251a69da93
garrepi <jacgarrett18@gmail.com>
2026-04-23 21:15:52 -0700

    add stuff to body

--- src/components/html-page.ts
+++ src/components/html-page.ts
@@ -5,18 +5,21 @@ import { navbar, type NavBarParams } from "./navbar";
 type HTMLPageParams = {
   params: { head: HeadParams; navbar?: NavBarParams };
   content: HTMLString;
+  scripts?: string[];
+  styleLinks?: string[];
 };
 
-export const htmlPage = ({ params, content }: HTMLPageParams): HTMLString => {
+export const htmlPage = ({ params, content, scripts, styleLinks }: HTMLPageParams): HTMLString => {
   return `
   <!DOCTYPE html>
   <html lang="en">
-    ${head(params.head)}
+    ${head({ ...params.head, extraLinks: styleLinks })}
     <body>
       ${params.navbar ? navbar(params.navbar) : ""}
       <main>
         ${content}
       </main>
+      ${scripts?.map((src) => `<script type="module" src="${src}"></script>`).join("\n") ?? ""}
     </body>
   </html>
   `;
