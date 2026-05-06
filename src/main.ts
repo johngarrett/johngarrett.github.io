4b75dd890a0cd44fa5d0894e88a215900e6b3a40
garrepi <jacgarrett18@gmail.com>
2026-05-06 13:45:08 -0700

    move JS output

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { ProjectPages } from "./projects/project-pages";
 import { HomePage } from "./home/home-page";
 import { StyleSheet } from "./styles/styles";
 import { build } from "./utils";
-import { ContentPages, fetchContent } from "./content";
+import { TripPages } from "./trips";
+import { fetchContent } from "./content";
 
 const scriptBuild = await Bun.build({
   entrypoints: ["./src/content/injected-scripts/gpx-views.ts"],
@@ -20,15 +22,9 @@ const renderables = [
   // main
   HomePage({ projects, trips }),
   // project pages
-  ...ContentPages(projects, {
-    path: "projects",
-    scripts: ["/js/gpx-views.js"],
-    styleLinks: ["/js/gpx-views.css"],
-  }),
+  ...ProjectPages(projects),
 
-  // trip pages
-  ...ContentPages(trips, {
-    path: "trips",
+  ...TripPages(trips, {
     scripts: ["/js/gpx-views.js"],
     styleLinks: ["/js/gpx-views.css"],
   }),
