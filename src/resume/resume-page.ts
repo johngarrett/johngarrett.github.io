15c1cd261e71551ab0da77a13f6df27938fb8ddd
garrepi <jacgarrett18@gmail.com>
2026-08-01 11:00:42 -0700

    add more jobs

--- src/resume/resume-page.ts
+++ src/resume/resume-page.ts
@@ -21,20 +21,53 @@ const jobs: Job[] = [
   {
     company: "Apple",
     title: "Senior Software Engineer",
-    oneline: "built and interactive map with custom map tiles",
+    oneline:
+      "worked on Apple TV and Apple Music for Smart TVs primarily as a framework engineer. i also built some UI and integrated with the client. I built features related to networking, payments, accounts, ratings, and other features",
     years: "2023-2026",
   },
   {
+    company: "Digital Light Meter",
+    title: "Hardware Project",
+    oneline: "this did things",
+    years: "2023",
+  },
+  {
     company: "Barrel Proof Apps",
-    title: "Software Engineer",
-    oneline: "built and interactive map with custom map tiles",
-    years: "2021-2023",
+    title: "Contract Software Engineer",
+    oneline:
+      "worked with a lot of different clients. react native apps, iOS apps, backend work, etc.",
+    years: "2020-2021",
   },
   {
     company: "Apple",
     title: "Intern",
-    oneline: "built and interactive map with custom map tiles",
+    oneline:
+      "worked in webassembly. migrating existing frameworks to new platforms",
     years: "summer 2021, summer 2022",
+  },
+  {
+    company: "SameTunes",
+    title: "Software Engineer",
+    oneline: "built a spotify cache, music match making software",
+    years: "2020",
+  },
+  {
+    company: "Kabbage",
+    title: "Mobile iOS Engineer",
+    oneline: "fintech company, built out features on the iOS app",
+    years: "2019",
+  },
+  {
+    company: "HyperSwift",
+    title: "DSL in swift",
+    oneline: "",
+    years: "2019",
+  },
+  {
+    company: "TrafficLight",
+    title: "iOS Engineer; co-founder",
+    oneline: "",
+    years: "2017-2019",
   },
 ];
 
