f384c7a72c9aa2567ec541695ee9f0d9bde5b4dc
garrepi <jacgarrett18@gmail.com>
2026-08-01 10:11:26 -0700

    add jobs

--- src/resume/resume-page.ts
+++ src/resume/resume-page.ts
@@ -1,8 +1,60 @@
 import { contentGrid, htmlPage, navBar } from "../components";
-import type { HTMLString, Renderable } from "../utils";
+import { html, type HTMLString, type Renderable } from "../utils";
+
+//<table>
+//  <thead>
+//    <tr>
+//      <th>Organization</th>
+//      <th>Role</th>
+//      <th>Years</th>
+//    </tr>
+//  </thead>
+//  <tbody>
+//    <tr>
+//      <td>Chick-fil-A</td>
+//      <td>Team Leader</td>
+//      <td>2014–2016</td>
+//    </tr>
+//    <tr>
+//      <td>Computers 2 Kids</td>
+//      <td>Volunteer</td>
+//      <td>2022–2023</td>
+//    </tr>
+//  </tbody>
+//</table>
+
+type TableParams = {
+  //head?: HTMLElement;
+  rows: HTMLString[]; // ["<td>foo</td>", "<td>bar</td>"]
+};
+
+const table = (params: TableParams): HTMLString => {
+  return html` <table>
+    <tbody>
+      ${params.rows.map((r) => `<tr>${r}</tr>`).join("")}
+    </tbody>
+  </table>`;
+};
+
+type Job = { company: string; title: string; years: string };
+const jobs: Job[] = [
+  {
+    company: "Apple",
+    title: "Senior Software Engineer",
+    years: "2023-2026",
+  },
+];
+
+const renderJob = (job: Job) => {
+  return `
+  <td>${job.company}</td>
+  <td>${job.title}</td>
+  <td>${job.years}</td>
+  `;
+};
 
 const resumeTable = (): HTMLString => {
-  return "foobar";
+  return table({ rows: jobs.map(renderJob) });
 };
 
 export const ResumePage = (): Renderable => {
